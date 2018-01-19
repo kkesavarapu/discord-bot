ba0421cf8c5ab63cb70306ba6c207df14ec2cc47
Kirti <kirti.kesavarapu@gmail.com>
2018-01-18 19:33:01 -0600

    fixed response format bugs

--- app/config/responses.js
+++ app/config/responses.js
@@ -1,7 +1,7 @@
 export const idle = [
     "I'm a fighter/rogue/sorceror/wizard/barbarian/bard multiclass",
     "I don't have enough skill points to help you with that",
-    "Let me roll a d20 to help you: `You roll a 1. Critical failure`",
+    "Let me roll a d20 to help you: **You roll a 1. Critical failure**",
     "The door is locked. I sit down.",
     "I attack the portcullis.",
     "Hold on..I'm busy switching classes."    
